10dd0052d6ee7967e973e816344676c84a4d7f0e
sugar1ofal <142041924+sugar1ofal@users.noreply.github.com>
2023-09-08 00:53:26 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,9 +9,7 @@
  */
 int sugar(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
-	write(1, message, 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
 
