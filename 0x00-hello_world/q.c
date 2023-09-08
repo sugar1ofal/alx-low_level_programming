eb51a301427407f36571248d8de99308dc991e86
Sugar <iwufavour306@gmail.com>
2023-09-07 22:44:35 -0700

    testingmore

---
+++ 0x00-hello_world/q.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+/**
+ * sugar - Entry point for the write command
+ *
+ * Description: This function prints a message.
+ *
+ * Return: 1 on success. Error code otherwise
+ */
+int sugar(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
+
+/**
+ * main - Entry point for me
+ *
+ * Return:  1 means sucess else error
+ */
+int main(void)
+{
+	sugar();
+	return (1);
+}
